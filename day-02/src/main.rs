7e818a812c0cafef7383010ef4e785d154a4991a
Manpreet Singh Matharu <matharumanpreet00@gmail.com>
2018-12-06 00:21:33 -0500

    added day-02 solution part 1

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // read input
+    let mut input = String::new();
+    std::io::stdin()
+        .read_to_string(&mut input)
+        .expect("Error reading input");
+
+
+    let solution = solve(&input);
+    println!("{}", solution);
+}
+
+
+fn solve(input: &String) -> i32 {
+    let mut twos = 0;
+    let mut threes = 0;
+
+    for id in input.lines() {
+        let mut counter = HashMap::new();
+
+        for c in id.chars() {
+            *counter.entry(c).or_insert(0) += 1;
+        }
+
+        if counter.values().find(|&&x| x == 2).is_some() { twos += 1 };
+        if counter.values().find(|&&x| x == 3).is_some() { threes += 1 };
+    }
+
+    twos * threes
 }
